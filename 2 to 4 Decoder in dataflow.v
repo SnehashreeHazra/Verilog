aafe1afc1c9356499c84e3fbc31b41832c73ff74
Snehashree Hazra <snehashreehazra783@gmail.com>
2023-09-29 20:17:10 +0530

    Delete 2 to 4 Decoder in dataflow.v

--- 2 to 4 Decoder in dataflow.v
+++
@@ -1,6 +0,0 @@
-module DEC24_DF (input E, [1:0]A, output [3:0]Y);
-  assign Y[3] = E & A[1] & A[0];
-  assign Y[2] = E & A[1] & (~A[0]);
-  assign Y[1] = E & (~A[1]) & A[0];
-  assign Y[0] =  E & (~A[1]) & (~A[0]);
-endmodule
